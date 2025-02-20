d4ff223658f899c80ff795dfe7231aed222d74a3
Racz Zoltan <racz.zoli@gmail.com>
2025-02-20 19:29:41 +0200

    Added parse_tokens() function call to main

--- main.c
+++ main.c
@@ -2,17 +2,23 @@
 #include <stdio.h>
 
 #include "lexer.h"
+#include "parser.h"
 
 char *source = "$a = 10;\n$b = 20;\n$x = $a + $b;\n$y = $b - $a;$str = \"hello world\";";
 
 int main()
 {
-	struct token_t *token = lexer_parse(source);
+	struct token_t *head = lexer_parse(source);
+	struct token_t *token = head;
 	
+	printf("\n******* LEXER INFO *******\n");
 	while (token) {
 		printf("token: \"%s\" (type: %s)\n", token->value, lexer_token_type_str(token->type));
 		token = token->next;
 	}
 	
+	printf("\n******* PARSE INFO *******\n");
+	parse_tokens(head);
+
 	return 0;
 }
