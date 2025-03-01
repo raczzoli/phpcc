94de2e6400d9774720688e460b2920f896152a0e
Racz Zoltan <racz.zoli@gmail.com>
2025-03-01 12:18:23 +0200

    Implemented symbol table structure and enums for types and scopes

--- symbol.c
+++ symbol.c
@@ -1,4 +1,13 @@
+#include <stdlib.h>
+
 #include "symbol.h"
 
-int symbol_insert(struct ast_node_t *var);
-int symbol_exists(struct ast_node_t *var);
+int symbol_insert(struct ast_node_t *var)
+{
+	return 0;
+}
+
+struct symbol *symbol_lookup(struct ast_node_t *var)
+{
+	return NULL;
+}
--- symbol.h
+++ symbol.h
@@ -1,7 +1,30 @@
 #ifndef SYMBOL_H
 #define SYMBOL_H
 
+#include "parser.h"
+
+enum symbol_types {
+	SYMB_TYPE_INTEGER = 1,
+	SYMB_TYPE_FLOAT,
+	SYMB_TYPE_STRING,
+	SYMB_TYPE_FUNCTION
+};
+
+enum symbol_scopes {
+	SYMB_SCOPE_GLOBAL = 1,
+	SYMB_SCOPE_LOCAL
+};
+
+struct symbol {
+	char *name;
+	enum symbol_types type;
+	enum symbol_scopes scope;
+
+	struct symbol *next;
+	struct symbol *prev;
+};
+
 int symbol_insert(struct ast_node_t *var);
-int symbol_exists(struct ast_node_t *var);
+struct symbol *symbol_lookup(struct ast_node_t *var);
 
 #endif //SYMBOL_H
